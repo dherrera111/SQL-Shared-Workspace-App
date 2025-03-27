599235ed2ff2f7986eb608cc34450ac89cb6abba
VictorLeung91 <victorleung91@gmail.com>
2025-03-27 17:38:09 -0600

    update

--- Tables/User.sql
+++ Tables/User.sql
@@ -1,7 +1,12 @@
 ﻿CREATE TABLE [dbo].[User]
 (
 	[UserId] INT NOT NULL PRIMARY KEY, 
-    [UserRoleId] VARCHAR(50) NOT NULL, 
+    [UserRoleId] INT NOT NULL, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [PhoneNumber] VARCHAR(50) UNIQUE NOT NULL, 
+    [EmailAddress] VARCHAR(50) UNIQUE NOT NULL, 
+    [PassWord] VARBINARY(250) NOT NULL, 
     CONSTRAINT [FK_User_UserRole] FOREIGN KEY ([UserRoleId]) REFERENCES [UserRole]([UserRoleId])
 
 )
--- Tables/WorkspaceType.sql
+++ Tables/WorkspaceType.sql
@@ -1,4 +1,6 @@
 ﻿CREATE TABLE [dbo].[WorkspaceType]
 (
-	[WorkspaceTypeId] INT NOT NULL PRIMARY KEY
+	[WorkspaceTypeId] INT NOT NULL PRIMARY KEY, 
+    [TypeName] VARCHAR(50) NOT NULL, 
+    CONSTRAINT [FK_WorkspaceType_Workspace] FOREIGN KEY ([WorkspaceTypeId]) REFERENCES [Workspace]([WorkspaceTypeId])
 )
